5aac0d93f338a377ad35cd589a76e4fde0d39ec0
Jorge Ramirez <joramirez96@gmail.com>
2020-10-08 22:35:07 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,7 +3,12 @@ class Song < ActiveRecord::Base
   belongs_to :genre
   has_many :notes
 
-  validates :title, :artist_id, :genre_id, presence: true
-  validates :title, uniqueness: { scope: [:artist_id]}
+  # validates :title, :artist_id, :genre_id, presence: true
+  # validates :title, uniqueness: { scope: [:artist_id]}
+
+  validates :artist_id, :genre_id, {presence: true}
+  validates :title, {presence: true, uniqueness: { scope: [:artist_id]}}
+
+  attr_accessor :artist_name
 
 end
